c09dbd9b01df791f9158f314d1a1f3f240d50fd8
boris <yuhaobo1990@gmail.com>
2016-02-25 22:18:59 +0800

    完成前两个习题

---
+++ ex_01_hello_world.py
@@ -0,0 +1,22 @@
+# coding=utf8
+
+# 习题1：第一个程序 A Good First Program
+
+# 中文问题：
+#
+# 如果注释中存在中文，python 解释时会出现ASCII报错
+# 解决这一问题，需要在文件第一行加上以下内容（双引号内的所有内容）：
+# "# -- coding: utf-8 --" 或
+# "# -- coding: utf8 --" 或
+# "# coding=utf-8" 或
+# "# coding=utf8"
+#
+# 注意：
+# 1. "utf-8" 或 "utf8" 一样可行
+# 2. 当 "# coding=utf8" 时，符号 "=" 两边不能有空格，否则报错。
+
+print "Hello World!"
+print "Hello Again"
+print "I like typing this."
+print "This is fun."
+print "Now is better than never."
