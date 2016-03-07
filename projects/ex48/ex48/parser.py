bbe29916b4eed75173cb82d382a94bb66b8636aa
borisyu <yuhaobo1990@gmail.com>
2016-03-07 09:41:01 +0800

    update

--- projects/ex48/ex48/parser.py
+++ projects/ex48/ex48/parser.py
@@ -30,3 +30,39 @@ def match(word_list, expecting):
 def skip(word_list, word_type):
     while peek(word_list) == word_type:
         match(word_list, word_type)
+
+def parse_verb(word_list):
+    skip(word_list, 'stop')
+
+    if peek(word_list) == 'verb':
+        return match(word_list, 'verb')
+    else:
+        raise ParserError("Expected a verb text")
+
+def parse_object(word_list):
+    skip(word_list, 'stop')
+    next_word = peek(word_list)
+
+    if next_word == 'noun':
+        return match(word_list, 'noun')
+    elif next_word == 'direction':
+        return match(word_list, 'direction')
+    else:
+        raise ParserError("Expected a noun or direction text")
+
+def parse_subject(word_list):
+    skip(word_list, 'stop')
+    next_word = peek(word_list)
+
+    if next_word == 'noun':
+        return match(word_list, 'noun')
+    elif next_word == 'verb':
+        return ('noun', 'player')
+    else:
+        raise ParserError("Expected a verb text")
+
+def parse_sentence(word_list):
+    subj = parse_subject(word_list)
+    verb = parse_verb(word_list)
+    obj = parse_object(word_list)
+    return Sentence(subj, verb, obj)
