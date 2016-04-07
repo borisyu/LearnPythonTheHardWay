79f99d96fc15165a12a49d3adf24ace19e7eb64a
borisyu <yuhaobo1990@gmail.com>
2016-04-07 17:57:04 +0800

    use webform

--- projects/microblog/app/views.py
+++ projects/microblog/app/views.py
@@ -1,6 +1,8 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from .forms import LoginForm
 
+# index page
 @app.route('/')
 @app.route('/index')
 def index():
@@ -16,3 +18,12 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+# login page
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="' + form.openid.data + '", remember_me=' + str(form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html', title='Sign In', form=form)
